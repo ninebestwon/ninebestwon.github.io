5891e7b8e83ebba221ce9837dccf8854c1d9ec2a
kyuwon416 <kyuwon416@gmail.com>
2025-10-17 12:52:37 +0900

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,7 +43,7 @@ window.addEventListener('scroll', scrollActive)
 /*===== CHANGE BACKGROUND HEADER =====*/ 
 function scrollHeader(){
     const nav = document.getElementById('header');
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+    if(this.scrollY >= 560) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader)
 
